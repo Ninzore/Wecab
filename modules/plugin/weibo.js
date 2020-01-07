6d8b0c93ca1d304486a766ed56ce76f7456b1ffd
Ninzore <33434617+Ninzore@users.noreply.github.com>
2020-01-07 23:05:21 +0800

    新增了订阅功能

--- modules/plugin/weibo.js
+++ modules/plugin/weibo.js
@@ -1,4 +1,8 @@
 var axios = require('axios');
+var mongodb = require('mongodb').MongoClient;
+
+var db_port = 27017;
+var db_path = "mongodb://127.0.0.1:" + db_port;
 
 function httpHeader(uid = 0, mid = 0) {
     let containerid = "107603" + uid;
@@ -90,53 +94,141 @@ function getWeibo(uid, num = 1, mid = 0) {
         params = httpHeader(uid).params;
     }
     return new Promise(resolve => {
-            axios({
-                method:'GET',
-                url: "https://m.weibo.cn/api/container/getIndex",
-                params : params,
-                headers: headers
-            }).then(response => {
-                if (num == -1) {
-                    for (let i=0; i<2; i++){
-                        if (response.data.data.cards[i].card_type == 9) {
-                            if ("isTop" in response.data.data.cards[i].mblog && response.data.data.cards[i].mblog.isTop == 1) {
-                                resolve(response.data.data.cards[i].mblog);
-                                return;
-                            }
-                            else{
-                                //console.log("没有置顶微博")
-                            }
+        axios({
+            method:'GET',
+            url: "https://m.weibo.cn/api/container/getIndex",
+            params : params,
+            headers: headers
+        }).then(response => {
+            if (num == -1) {
+                for (let i=0; i<2; i++){
+                    if (response.data.data.cards[i].card_type == 9) {
+                        if ("isTop" in response.data.data.cards[i].mblog && response.data.data.cards[i].mblog.isTop == 1) {
+                            resolve(response.data.data.cards[i].mblog);
+                            return;
+                        }
+                        else{
+                            console.log("没有置顶微博")
                         }
                     }
                 }
-                else {
-                    let count = 0;
-                    for (let i=0; i<9; i++){
-                        if (response.data.data.cards[i].card_type == 9) {
-                            if ("isTop" in response.data.data.cards[i].mblog && response.data.data.cards[i].mblog.isTop == 1) {
-                                count--;
-                            }
-                            if (count == num) {
-                                resolve(response.data.data.cards[i].mblog)
-                            }
-                            count++;
+            }
+            else {
+                let count = 0;
+                let card_num_seq = [];
+                for (let i=0; i<9; i++){
+                    if (response.data.data.cards[i].card_type == 9) {
+                        if ("isTop" in response.data.data.cards[i].mblog && response.data.data.cards[i].mblog.isTop == 1) {
+                            count--
                         }
+                        card_num_seq.push({card_num: i, mid : response.data.data.cards[i].mblog.mid});
+                        // console.log(card_num_seq)
+                        if (count == num) {
+                            card_num_seq.sort(function(a,b){
+                                if (a.mid > b.mid) return -1;
+                                else return 1;
+                            })
+                            // console.log(card_num_seq)
+                            resolve(response.data.data.cards[card_num_seq[num].card_num].mblog)
+                            return 0;
+                        } 
+                        count++;
                     }
                 }
-            })
-            .catch(() => {
-                //console.log(error);
-            });
+            }
+        })
+        .catch(() => {
+            //console.log(error);
         });
+    });
 }
 
-function checkWeibo(choose = 1, num = 1) {
-    return new Promise ((resolve) =>
-        getWeibo(choose, 0, num).then(mblog => {
-            resolve (mblog.id);
-        })
-    );
+async function addSubscribe(context, replyFunc, choose = null, name = null) {
+    let uid = false;
+    let group_id = context.group_id;
+    let user_id = context.user_id;
+    if (choose > 0) uid = await getUserId("", choose);
+    else if (name.length > 1) uid = await getUserId(name, 0);
+    else replyFunc(context, "你名字写了吗", true);
+    if (!uid) replyFunc(context, "你名字写对了吗", true);
+    else {
+        let mblog = await getWeibo(uid);
+        let mid = mblog.mid;
+        let screen_name = mblog.user.screen_name;
+        mongodb(db_path, {useUnifiedTopology: true}).connect((err, mongo) => {
+            if (err) replyFunc(context, "database openning error", true);
+            else {
+                let text = "";
+                let coll = mongo.db('bot').collection('weibo');
+                coll.updateOne({weibo_uid : uid},
+                               {$set : {mid : mid},
+                                $addToSet : {groups : {group_id : group_id, user_id : user_id}}},
+                               {upsert:true}, (err) => {
+                                    if (err) text = "database subscribes update error";
+                                    else text = "已订阅" + screen_name;
+                                    replyFunc(context, text, true);
+                                });
+                mongo.close();
+            }
+        });
+    }
 }
+
+async function rmSubscribe(context, replyFunc, choose = null, name = null) {
+    let uid = false;
+    if (choose > 0) uid = await getUserId("", choose);
+    else if (name.length > 1) uid = await getUserId(name, 0);
+    else replyFunc(context, "你名字写了吗", true);
+    if (!uid) replyFunc(context, "你名字写对了吗", true);
+    else {
+        let mblog = await getWeibo(uid);
+        let screen_name = mblog.user.screen_name;
+        mongodb(db_path, {useUnifiedTopology: true}).connect((err, mongo) => {
+            if (err) replyFunc(context, "database openning error", true);
+            else {
+                let text = "";
+                let coll = mongo.db('bot').collection('weibo');
+                coll.findOneAndDelete({weibo_uid : uid}, (err) => {
+                                    if (err) text = "database subscribes update error";
+                                    else text = "已取消订阅" + screen_name;
+                                    replyFunc(context, text, true);
+                                });
+                mongo.close();
+            }
+        });
+    }
+}
+
+function checkWeibo(replyFunc) {
+    setInterval(() => {
+        mongodb(db_path, {useUnifiedTopology: true}).connect(async function(err, mongo) {
+            if (err) console.log("error");
+            else {
+                let coll = mongo.db('bot').collection('weibo');
+                let subscribes = await coll.find({}, {projection: {_id: 0}}).toArray();
+                for (let i = 0; i < subscribes.length; i++) {
+                    let mblog = await getWeibo(subscribes[i].weibo_uid);
+                    let last_mid = subscribes[i].mid;
+                    let current_mid = mblog.mid;
+                    if (current_mid > last_mid) {
+                        let groups = subscribes[i].groups;
+                        groups.forEach(group => {
+                            weiboSender(group, replyFunc, mblog);
+                        });
+                        coll.updateOne({weibo_uid : subscribes[i].weibo_uid},
+                                       {$set : {mid : current_mid}}, 
+                                       (err) => {
+                                           if (err) replyFunc(group, "database update error during checkWeibo", true);
+                                       });
+                        // console.log(current_mid);
+                    }
+                }
+                mongo.close();
+            }
+        });
+    }, 5 * 60000);
+}
+
 
 function textFilter(text) {
     // console.log(text)
@@ -151,30 +243,41 @@ function textFilter(text) {
                 .replace(/<img style=.*?>/g, "")
                 .replace(/<span.+?span>/g, "")
                 .replace(/<\/a>/g, "")
-                .replace(/<br \/>/g , " ")
+                .replace(/<br \/>/g , "\n")
                 .replace(/&quot;/g , "'")
                 .replace(/网页链接/g, "")
                 .replace(/\\.*?秒拍视频/g, "");
 }
 
-async function rtWeibo(context, replyFunc, choose, name = "", num = 1, uid = 0, mid=0) {
+async function rtWeibo(context, replyFunc, choose = 1, name = "", num = 0, uid = 0, mid=0) {
+    // getUserId(name, choose).then(uid => {
+    //     console.log(uid)
     let mblog;
-    if (uid != 0 || mid != 0) {
+    if (uid != 0 && mid != 0) {
         mblog = await getWeibo(uid, num, mid);
+    }
+    else if (uid != 0 && mid == 0) {
+        mblog = await getWeibo(uid, num);
     }
     else {
         let uid = await getUserId(name, choose);
         mblog = await getWeibo(uid, num);
     }
+    weiboSender(context, replyFunc, mblog);
+}
+
+async function weiboSender(send_target, replyFunc, mblog) {
     let text = textFilter(mblog.text);
+    // console.log(mblog)
+    // let text = textFilter(mblog.page_info.content2);
     let id = mblog.id;
     if ("pics" in mblog) {
         let pics = mblog.pics;
-        for (var pic of pics) {
+        for (let pic of pics) {
             pid = pic.pid;
             pic_url = pic.large.url;
-            let payload = "[CQ:image,cache=0,file=" + pic_url + "]";
-            replyFunc(context, payload);
+            payload = "[CQ:image,cache=0,file=" + pic_url + "]";
+            replyFunc(send_target, payload);
         }
     }
     if ("page_info" in mblog) {
@@ -197,77 +300,77 @@ async function rtWeibo(context, replyFunc, choose, name = "", num = 1, uid = 0, 
                 media_src = "视频地址: " + media.stream_url;
             }
             let payload = "[CQ:image,cache=0,file=" + mblog.page_info.page_pic.url + "]";
-            replyFunc(context, payload);
-            replyFunc(context, media_src);
+            replyFunc(send_target, payload);
+            replyFunc(send_target, media_src);
         }
     }
         if ("retweeted_status" in mblog) {
             let rt_user_info = mblog.retweeted_status.user.screen_name;
-            rtWeiboDetail(mblog.retweeted_status.id, rt_user_info);
+            rtWeiboDetail(send_target, replyFunc, mblog.retweeted_status.id, rt_user_info);
             if ("page_info" in mblog.retweeted_status) {
                 let rt_page_info = mblog.retweeted_status.page_info;
                 if ("media_info" in rt_page_info){
-                    let rt_media = mblog.retweeted_status.page_info.media_info;
-                    let rt_media_src;
-                    if ("hevc_mp4_hd" in rt_media && rt_media.hevc_mp4_hd != "") {
-                        media_src = "视频地址: " + rt_media.hevc_mp4_hd;
-                    }
-                    else if ("h265_mp4_hd" in rt_media && rt_media.h265_mp4_hd != "") {
-                        media_src = "视频地址: " + rt_media.h265_mp4_hd;
-                    }
-                    else if ("mp4_720p_mp4" in rt_media && rt_media.mp4_720p_mp4 != "") {
-                        rt_media_src = " 转发视频地址: " + rt_media.mp4_720p_mp4;
-                    }
-                    else if ("mp4_hd_url" in rt_media && rt_media.mp4_hd_url != "") {
-                        rt_media_src = " 转发视频地址: " + rt_media.mp4_hd_url;
-                    }
-                    else {
-                        rt_media_src = " 转发视频地址: " + rt_media.stream_url;
-                    }
-                    let payload = "[CQ:image,cache=0,file=" + rt_page_info.retweet_video_pic_url + "]";
-                    replyFunc(context, payload);
-                    replyFunc(context, rt_media_src);
-                }
+                let rt_media = mblog.retweeted_status.page_info.media_info;
+                let rt_media_src = "";
+            if ("mp4_720p_mp4" in rt_media) {
+                rt_media_src = " 转发视频地址: " + rt_media.mp4_720p_mp4;
             }
+            else if ("mp4_hd_url" in rt_media) {
+                rt_media_src = " 转发视频地址: " + rt_media.mp4_hd_url;
+            }
+            else {
+                rt_media_src = " 转发视频地址: " + rt_media.stream_url;
+            }
+            let payload = "[CQ:image,cache=0,file=" + rt_page_info.retweet_video_pic_url + "]";
+            replyFunc(send_target, payload);
+            replyFunc(send_target, rt_media_src);
+        }
+    }
     
     if ("pics" in mblog.retweeted_status) {
         for (var pic of mblog.retweeted_status.pics) {
             pid = pic.pid;
             pic_url = pic.large.url;
             let payload = "[CQ:image,cache=0,file=" + pic_url + "]";
-            replyFunc(context, payload);
+            replyFunc(send_target, payload);
         }
     }
         }
         
     if (/\.\.\.全文/.exec(text)) {
         //console.log(查看全文);
-        rtWeiboDetail(id);
-        return
+        rtWeiboDetail(send_target, replyFunc, id);
+        return 0;
     }
     text = mblog.user.screen_name + ":\n" + text;
-    replyFunc(context, text);
+    replyFunc(send_target, text);
 }
 
-function rtWeiboDetail(context, replyFunc, id, rt_user_info = "") {
-    axios({
-        method:'GET',
-        url: "https://m.weibo.cn/detail/" + id,
-        headers : {
-            "Host": "m.weibo.cn",
-            "Referer": "https://m.weibo.cn/u/" + id,
-            "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148",
-            "Accept":'application/json',
-            "X-Requested-With":"XMLHttpRequest"
-        }
-    }).then(response => {
-        // console.log(response.data)
-        let text = /text": "(.*)\"/.exec(response.data);
-        text = "转发自：" + rt_user_info + "\n" + textFilter(text[1]);
-        replyFunc(context, text);
-    })
-    .catch(() => {
-        //console.log(error);
+function rtWeiboDetail(context, replyFunc, id, rt_user_info = null) {
+    // console.log(id)
+    return new Promise(resolve => {
+        axios({
+            method:'GET',
+            url: "https://m.weibo.cn/detail/" + id,
+            headers : {
+                "Host": "m.weibo.cn",
+                "Referer": "https://m.weibo.cn/u/" + id,
+                "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148",
+                "Accept":'application/json',
+                "X-Requested-With":"XMLHttpRequest"
+            }
+        }).then(response => {
+            // console.log(response.data)
+            let text = /text": "(.*)\"/.exec(response.data);
+            if (rt_user_info != null){
+                text = "转发自：" + rt_user_info + "\n" + textFilter(text[1]);
+            }
+            else text = textFilter(text[1]);
+            replyFunc(context, text);
+        })
+        .catch(() => {
+            //console.log(error);
+        });
     });
 }
 
@@ -275,9 +378,7 @@ function rtWeiboByUrl(context, replyFunc, url){
     let temp = /https:\/\/m.weibo.cn\/(\d+)\/(\d+)/.exec(url);
     let user_id = temp[1];
     let mid = temp[2];
-    // console.log(user_id);
-    // console.log(mid);
     rtWeibo(context, replyFunc, choose = 0, name = "", num = 0, uid = user_id, mid = mid);
 }
 
-module.exports = {rtWeibo, rtWeiboByUrl};
+module.exports = {rtWeibo, rtWeiboByUrl, addSubscribe, rmSubscribe, checkWeibo};
