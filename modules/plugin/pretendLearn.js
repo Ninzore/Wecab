935fb8346193d2a53fb51b3a84f7153d15eef5b8
Ninzore <33434617+Ninzore@users.noreply.github.com>
2020-03-23 00:51:49 +0000

    Update pretendLearn.js

--- modules/plugin/pretendLearn.js
+++ modules/plugin/pretendLearn.js
@@ -14,7 +14,7 @@ function teach(context, replyFunc) {
     if (result == undefined) {
         result = context.message.match(qa_common);
     }
-    if (result != null && "group_id" in context) {
+    if (result != null) {
         let {groups : {qes, ans, mode_name}} = result;
         // console.log(qes)
         // console.log(groups)
@@ -72,14 +72,10 @@ function teach(context, replyFunc) {
                 let qa_set = mongo.db('qa_set').collection("qa" + String(context.group_id));
                 await qa_set.updateOne({question : qes, mode : mode}, {$addToSet : {answers : ans}}, {upsert : true});
                 mongo.close();
-                text = "好我会了";
-                sender(replyFunc, context, text);
-            }).catch((e) => {
-                console.log(e);
-                text = "脑子不够用，没学会";
-                sender(replyFunc, context, text);
-            });
+            }).catch((e) => {console.log(e)});
+            text = "好我会了";
         }
+        sender(replyFunc, context, text);
         return true;
     }
     else return false;
@@ -94,7 +90,7 @@ function remember(context, replyFunc) {
         match_result = context.message.match(common);
     }
 
-    if (match_result != null && "group_id" in context) {
+    if (match_result != null) {
         let {groups : {ans, mode_name}} = match_result;
         let text = "";
         let result = "";
@@ -145,7 +141,7 @@ function forget(context, replyFunc) {
         match_result = context.message.match(common);
     }
 
-    if (match_result != null && "group_id" in context) {
+    if (match_result != null) {
         let {groups : {qes, mode_name}} = match_result;
         let text = "";
         let result = "";
@@ -228,9 +224,25 @@ function talk(context, replyFunc) {
     }).catch((err) => {console.log(err)});
 }
 
+function learn(context, replyFunc) {
+    if ("group_id" in context) {
+        if (teach(context, replyFunc)) {
+            return true;
+        }
+        else if (forget(context, replyFunc)) {
+            return true;
+        }
+        else if (remember(context, replyFunc)) {
+            return true
+        }
+        else return false;
+    }
+    else return false;
+}
+
 function sender(replyFunc, context, text) {
     // console.log(text);
     replyFunc(context, text);
 }
 
-module.exports = {teach, talk, forget, remember};
+module.exports = {learn, talk};
