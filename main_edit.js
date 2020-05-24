44e7e185d7680bac2a816884140e44a4d4386999
Ninzore <33434617+Ninzore@users.noreply.github.com>
2020-05-25 00:01:09 +0100

    Delete main_edit.js

--- main_edit.js
+++
@@ -1,7 +0,0 @@
-else if (pixivImage.pixivCheck(context, replyMsg, bot));
-else if (weibo.weiboCheck(context, replyMsg));
-else if (bilibili.bilibiliCheck(context, replyMsg));
-else if (pokemon.pokemonCheck(context, replyMsg));
-else if (/^\.dice.+/g.exec(context.message)) {
-    dice(context, replyMsg, rand);
-}
