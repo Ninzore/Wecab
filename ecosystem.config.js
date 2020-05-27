8081a85b8fc82b386d237421018fe632f9a96553
Ninzore <33434617+Ninzore@users.noreply.github.com>
2020-05-27 22:48:04 +0100

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     apps: [
         {
-            name: 'CQPF',
+            name: 'Wecab',
             script: 'index.js',
             instances: 1,
             autorestart: true,
