ab508a329ad91ab820bb0aa426fc67db48e3709f
Ninzore <33434617+Ninzore@users.noreply.github.com>
2021-01-22 23:04:15 +0000

    feat: 可以自行禁止接收消息了

--- modules/plugin/telephone.js
+++ modules/plugin/telephone.js
@@ -1,5 +1,5 @@
 let bot;
-let replyFunc = (context, msg, at = false) => {};
+let replyFunc = {};
 let yellow_book = [];
 let black_book = [];
 
@@ -15,7 +15,7 @@ function init(replyMsg, main_bot) {
     }, 500);
 }
 
-async function connect(context) {
+async function calling(context) {
     if (context.message.length > 200) {
         replyFunc(context, "太长了");
         return;
@@ -27,6 +27,9 @@ async function connect(context) {
     const receiver = temp[1];
     if (receiver == sender_group) {
         replyFunc(context, "不能自己给自己发");
+        return;
+    } else if (black_book.some(value => value == receiver)) {
+        replyFunc(context, "该地址已禁用连线");
         return;
     }
 
@@ -44,10 +47,37 @@ async function connect(context) {
     replyFunc({group_id : receiver, message_type : "group"}, message);
 }
 
+function connect(context) {
+    if (context.sender.role == "member") {
+        replyFunc(context, "不准");
+        return;
+    }
+    black_book = black_book.filter(value => value != context.group_id);
+    replyFunc(context, "又能收到消息了");
+    return;
+}
+
+function disconnect(context) {
+    if (context.sender.role == "member") {
+        replyFunc(context, "不准");
+        return;
+    }
+    black_book.push(context.group_id);
+    replyFunc(context, "不会再收到消息了");
+    return;
+}
+
 function paging(context) {
-    if (!black_book.some(value => value == context.group_id) && 
-        /^发送\d{8,10}[,，].+/.test(context.message)) {
+    if (/^发送\d{8,10}[,，].+/.test(context.message)) {
+        calling(context);
+        return true;
+    }
+    else if (/^开启跨组消息$/.test(context.message)) {
         connect(context);
+        return true;
+    }
+    else if (/^关闭跨组消息$/.test(context.message)) {
+        disconnect(context);
         return true;
     }
     else if (/^通信列表$/.test(context.message)) {
