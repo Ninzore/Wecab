9b869cf4143d0b0f347f3b63c5dd24387e861cc1
Ninzore <33434617+Ninzore@users.noreply.github.com>
2021-04-08 23:03:11 +0100

    Fix: promisify undefined
    Style: 去除不必要的代码

--- utils/gifcheck.js
+++ utils/gifcheck.js
@@ -1,17 +1,17 @@
 const logger2 = require('./logger2'); //日志功能
-const exec2 = promisify(require('child_process').exec);
+const promisify = require("util").promisify;
+const exec = promisify(require('child_process').exec);
 
 /** 通过ffmpeg判断gif总帧数多少*/
-module.exports = function gifCheck(dir) {
-    return await new Promise(async (resolve, reject) => {
-        exec2(`ffmpeg -i ${dir}/temp.gif -f null -`) //判断gif的总帧数 https://www.npmjs.com/package/gif-meta https://github.com/indatawetrust/gif-meta
+module.exports = async function gifCheck(dir) {
+    return new Promise(async (resolve, reject) => {
+        exec(`ffmpeg -i ${dir}/temp.gif -f null -`) //判断gif的总帧数 https://www.npmjs.com/package/gif-meta https://github.com/indatawetrust/gif-meta
             .then(async giftemp => {
-                let giftemp2 = /frame=(.+?)fps/.exec(JSON.stringify(giftemp.stderr))[1].replace("fps", "").trim();
-                //logger2.info("gif的总帧数:" + giftemp2);
+                let giftemp2 = /frame=(.+?)fps/.exec(giftemp.stderr)[1].replace("fps", "").trim();
                 resolve(giftemp2);
             }).catch(err => {
-                logger2.error("gif的总帧数:" + err);
-                resolve(null);
+                logger2.error("gifCheck " + err);
+                resolve();
             });
     });
 }
