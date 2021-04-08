89f0e8fbe33359b69836a0a95860e7289019df15
Ninzore <33434617+Ninzore@users.noreply.github.com>
2021-04-08 22:29:46 +0100

    Refactor: 调整代码结构
    Chore: 调整失败时的log level

--- utils/Downloadx.js
+++ utils/Downloadx.js
@@ -1,14 +1,12 @@
 const fs = require('fs-extra');
 const path = require('path');
-const logger2 = require('./logger2'); //日志功能
+const logger2 = require('./logger2');
 const Axios = require('axios');
 const HttpsProxyAgent = require("https-proxy-agent");
 const MD5 = require('js-md5');
 const PROXY = global.config.proxy;
-const join = path.join;
 
 let axios = false;
-//console.log(PROXY);
 if (PROXY.startsWith("http")) {
     axios = Axios.create({
         proxy: false,
@@ -16,92 +14,58 @@ if (PROXY.startsWith("http")) {
     });
 }
 else axios = Axios;
-/**
- * 
- * @param startPath  起始目录文件夹路径
- * @returns {Array}
- */
-//https://www.imooc.com/wenda/detail/459466 nodejs的FS或path如何获取某文件夹下的所有文件的文件名呢。
-function findSync(startPath) {
-    let result = [];
 
-    function finder(path) {
-        let files = fs.readdirSync(path);
-        files.forEach((val, index) => {
-            let fPath = join(path, val);
-            let stats = fs.statSync(fPath);
-            //if(stats.isDirectory()) finder(fPath);
-            if (stats.isFile()) {
-                //logger.info(fPath);
-                result.push(fPath);
-            }
-        });
-    }
-    finder(startPath);
-    return result;
-}
-module.exports = async function Downloadx(url, pic = true) {
+async function Downloadx(url) {
     let name = MD5(url);
-    logger2.info("下载文件 , " + url + " , 转成md5值:" + name);
-    let path2 = path.join(__dirname, `../tmp/pic`);
-    let path3 = path.join(__dirname, `../tmp/video`);
-    /*if (fs.existsSync(path2) == false) {//没有tmp文件夹就创建文件夹
-        fs.mkdirSync(path2);
-    }*/
-    /*let temp = await findSync(path2);
-    let tmp = "";
-    for (let i = 0; i < temp.length; i++) {
-        tmp = temp[i].split(".")[0].split("\\tmp\\")[1] || temp[i].split(".")[0].split("/tmp/")[1];//windows路径+linux路径
-        //logger2.info(tmp + "," + name);
-        if (name == tmp) {
-            logger2.info("找到文件:" + temp[i]);
-            return temp[i];//如果图片或视频已经存在则直接返回路径
+    logger2.info(["下载文件", url, "目标文件名", name].join(", "));
+    
+    //如果已经存在则直接返回路径
+    for (let ext of ["jpg", "mp4"]) {
+        let tmpdir = ext == "jpg" ? "./tmp/pic" : "./tmp/video";
+        if (!fs.existsSync("./tmp")) fs.mkdirSync("./tmp");
+        if (!fs.existsSync(tmpdir)) fs.mkdirSync(tmpdir);
+        
+        let tmp = path.join(tmpdir, `${name}.${ext}`);
+        if (fs.existsSync(tmp)) {
+            logger2.info("已存在文件: " + tmp);
+            return tmp;
         }
-    }*/
-    let tmp = path.resolve(path2, `${name}.jpg`);
-    if (fs.existsSync(tmp) == true) {
-        logger2.info("找到文件:" + tmp);
-        return tmp;//如果图片已经存在则直接返回路径
     }
-    tmp = path.resolve(path3, `${name}.mp4`);
-    if (fs.existsSync(tmp) == true) {
-        logger2.info("找到文件:" + tmp);
-        return tmp;//如果视频已经存在则直接返回路径
-    }
+    
     const response = await axios({
         url,
         method: "GET",
         responseType: "stream",
-        timeout: 30000,
+        timeout: 3000,
     }).catch(err => {
-        logger2.info(new Date().toString() + ",获取下载资源失败:" + err);
+        logger2.warn("下载资源失败:", err);
         return false;
     });
-    if (response != false) {
+
+    if (response) {
         const fileType = response.headers["content-type"].split("/")[1].replace("jpeg", "jpg");
-        logger2.info("content-type:" + fileType);
         let mypath = "";
-        if (fileType == "jpg") {
-            mypath = path.resolve(path.join(__dirname, `../tmp/pic`), `${name}.${fileType}`);
-        }
-        else {
-            mypath = path.resolve(path.join(__dirname, `../tmp/video`), `${name}.${fileType}`);
-        }
+        let filename = `${name}.${fileType}`;
+
+        if (fileType == "jpg") mypath = path.join("./tmp/pic", filename);
+        else if (fileType == "mp4") mypath = path.join("./tmp/video", filename);
+        else return false;
+
         const writer = fs.createWriteStream(mypath);
         response.data.pipe(writer);
-        return await new Promise(async (resolve, reject) => {
-            writer.on("finish",
-                data => {
-                    logger2.info(new Date().toString() + ",下载资源成功:" + JSON.stringify(data));
-                    resolve(mypath);
-                });
-            writer.on("error",
-                err => {
-                    logger2.error(new Date().toString() + ",下载资源失败: " + JSON.stringify(err));
-                    resolve("");
-                });
+        return new Promise((resolve, reject) => {
+            writer.on("finish", () => {
+                logger2.info("下载资源成功: " + filename);
+                resolve(mypath);
+            });
+            writer.on("error", err => {
+                logger2.warn("下载资源失败: " + url, err);
+                reject(err);
+            });
         });
     } else {
-        return "";
+        return false;
     }
 }
+
+module.exports = Downloadx;
