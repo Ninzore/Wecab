6ddf1450f064792683c00af2be2c7c56bfd480b6
Ninzore <33434617+Ninzore@users.noreply.github.com>
2021-04-08 22:47:25 +0100

    Feat: 更多的log info
    Feat: 不再需求传入文件类型

--- utils/sizeCheck.js
+++ utils/sizeCheck.js
@@ -1,10 +1,11 @@
 const Axios = require('axios');
+const logger2 = require('./logger2');
 const HttpsProxyAgent = require("https-proxy-agent");
 
 const PIC_MAX_SIZE = 30 * 1024 * 1024; //图片最大体积
 const VID_MAX_SIZE = 100 * 1024 * 1024; //视频最大体积
 let axios = false;
-//console.log(PROXY);
+
 if (PROXY.startsWith("http")) {
     axios = Axios.create({
         proxy: false,
@@ -14,23 +15,24 @@ if (PROXY.startsWith("http")) {
 else axios = Axios;
 
 /** 通过链接判断文件大小*/
-module.exports = function sizeCheck(url, pic = true) { //true 图片 false 视频
+module.exports = async function sizeCheck(url) {
     return axios({
         method: "head",
         url: url,
         headers: {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
         },
-        timeout: 15000
+        timeout: 3000
     }).then(res => {
-        //console.log(JSON.stringify(res.headers));
-        if (pic == true) {
-            return parseInt(res.headers["content-length"]) < PIC_MAX_SIZE ? true : ((res.headers["content-length"] / 1024 / 1024) + "MB"); //图片
-        } else {
-            return parseInt(res.headers["content-length"]) < VID_MAX_SIZE ? true : ((res.headers["content-length"] / 1024 / 1024) + "MB"); //视频
+        let filetype = res.headers["content-type"];
+        let max_size = filetype == "image/jpeg" ? PIC_MAX_SIZE : VID_MAX_SIZE
+        if (parseInt(res.headers["content-length"]) < max_size) return true;
+        else {
+            logger2.warn([url, filetype, "exceed maximum allowed size", max_size].join(" "));
+            return false;
         }
     }).catch(err => {
-        console.error(new Date().toString() + ",sizeCheck:" + url + "," + err);
-        return "获取文件大小失败";
+        logger2.error("sizeCheck:" + url, err);
+        return false;
     });
 }
