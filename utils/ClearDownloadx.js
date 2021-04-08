49f0f9d99305cbb53e9f262818d578423d567fe9
Ninzore <33434617+Ninzore@users.noreply.github.com>
2021-04-08 21:08:33 +0100

    Feat: more logger info
    Chore: 删除多余注释

--- utils/ClearDownloadx.js
+++ utils/ClearDownloadx.js
@@ -1,27 +1,12 @@
 const fs = require('fs-extra');
-//const logger2 = require('./logger2'); //日志功能
-//https://www.jb51.net/article/127906.htm nodejs判断文件、文件夹是否存在及删除的方法
-
-//删除所有的文件(将所有文件夹置空)
-/*function emptyDir(filePath) {
-    const files = fs.readdirSync(filePath)//读取该文件夹
-    files.forEach((file) => {
-        const nextFilePath = `${filePath}/${file}`
-        const states = fs.statSync(nextFilePath)
-        if (states.isDirectory()) {
-            emptyDir(nextFilePath)
-        } else {
-            fs.unlinkSync(nextFilePath)
-            //logger2.info(`删除文件 ${nextFilePath} 成功`)
-        }
-    })
-}*/
-//https://leotian.cn/posts/ee16/
+const logger2 = require('./logger2');
 
 module.exports = async function ClearDownloadx() {
     return await new Promise(function (resolve, reject) {
+        logger2.info("Clean /tmp/pic");
         fs.emptyDir('./tmp/pic');
+        logger2.info("Clean /tmp/video");
         fs.emptyDir('./tmp/video');
-        resolve(null);
+        resolve();
     });
 }
