d6c8528aa82c6ab2525db4e0daee2fb87ba73968
Ninzore <33434617+Ninzore@users.noreply.github.com>
2021-04-10 12:50:16 +0100

    Fix: tmp文件夹未成功创建

--- utils/mkdirTmp.js
+++ utils/mkdirTmp.js
@@ -1,9 +1,8 @@
 const path = require('path');
 const fs = require('fs');
 
-module.exports = function () {
-    const tmpDir = path.join(__dirname, '../tmp/pic');
-    const tmpDir2 = path.join(__dirname, '../tmp/video');
-    if (!fs.existsSync(tmpDir)) fs.mkdirSync(tmpDir);
-    if (!fs.existsSync(tmpDir2)) fs.mkdirSync(tmpDir2);
+module.exports = function mkdirTmp() {
+    for (let dir of ["./tmp", "./tmp/pic", "./tmp/video"]) {
+        if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+    }
 }
